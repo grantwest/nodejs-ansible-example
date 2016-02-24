c9686ec4eea276e05c6cbe4f55cbf8a5917d0539
Grant West <g.west@me.com>
2016-02-24 00:20:30 -0600

    initial commit

---
+++ hello-app/app.js
@@ -0,0 +1,22 @@
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+sleep(3000); //This is to make the application take longer to start up for demonstration purposes. 
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
